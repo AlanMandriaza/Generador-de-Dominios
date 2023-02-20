f9cb2c65e3d43e42d36c944e65e79d7312820d66
AlanMandriaza <alan.mandriaza@gmail.com>
2023-02-20 01:25:46 +0000

    terminado

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let domain = ['.com', '.es', '.net', '.io', '.cl', '.ar', '.co', '.de'];
 
 let maxPronounIndex = pronoun.length - 1;
 let maxAdjIndex = adj.length - 1;
 let maxNounIndex = noun.length - 1;
+let maxDomainIndex = domain.length - 1;
 
-let generateExcuse = () => {
+let generateDomain = () => {
   let whoindex = Math.floor(Math.random() * (maxPronounIndex + 1));
   let whatindex = Math.floor(Math.random() * (maxAdjIndex + 1));
   let whenindex = Math.floor(Math.random() * (maxNounIndex + 1));
+  let domainindex = Math.floor(Math.random() * (maxDomainIndex + 1));
 
-  return pronoun[whoindex] + " " + adj[whatindex] + " " + noun[whenindex];
+  return "www." + pronoun[whoindex] + adj[whatindex] + noun[whenindex] + domain[domainindex];
 };
 
-let excuse = generateExcuse();
-console.log(excuse);
+let domainName = generateDomain();
+console.log(domainName);
